5de28f990883f88850635474fa0e4e2996b68457
tkf <kaifang.tang@henganpros.com>
2020-12-21 22:23:18 +0800

    fixed: npm提交

--- 架构/6.module/1.pre.js
+++ 架构/6.module/1.pre.js
@@ -2,12 +2,52 @@
     // 1. eval // eval执行会向上查找变量,产生作用域问题
         // var a = 1
         // eval('console.log(a)') 
-    // 2. new Function传参 
+    // 2. new Function传参 (vue源码用的new Function)
       // 2.1不会像上级查找变量(干净环境)
             // let a = 1
             // let func = new Function('console.log(a)')
             // func()
       // 2.2最后一个参数是函数需要转换的,前面都是传给参数的变量
-            let a = 2
-            let func = new Function('a','console.log(a)')
-            func(100)
+            // let a = 2
+            // let func = new Function('a','console.log(a)')
+            // func(100)
+    // 3. node实现字符串执行是用自己的核心模块支持,而不是new Function/eval
+
+// node分模块为三类
+// (1)  第三方模块
+//     需要安装,使用是用require引入
+
+// (2) 自定义文件模块
+//     任何一个自己写的js文件
+// (3) 核心模块 可以直接使用
+//     fs/http/path/vm/虚拟机模块
+    // 1) 虚拟机
+        // let vm = require('vm')  // 使用的很少,都是源码内部使用
+        // let a = 1
+        // vm.runInThisContext("console.log(100)") // 用法像eval,但是不能获取全局变量,相当于一个内部沙箱环境
+    // 2) 文件模块(读取的文件不存在就会报错,写入文件不存在就会创建)
+    let fs = require('fs')
+    let book = fs.readFileSync('./age.txt','utf-8') // 同步阅读函数 返回内容 第一个参数是路径,第二个是编码
+    let existUrl= fs.existsSync('./age.txt') // 同步读取文件是否存在,返回的是boolean 
+    console.log(existUrl);
+    console.log(book);
+    // 3) path模块
+        // path模块,和fs模块一般组合使用 resolve dirname join extname
+            let path = require('path')
+            console.log(__dirname);
+            let currentPath = path.resolve(__dirname,'age.txt') // 解析路径,如果碰到/ 就会按照根目录来解析,直接到c盘了
+            let currentPath1 = path.join(__dirname,'age.txt') // 拼接路径 碰到/ 只是拼接
+            let parent = path.dirname('./a/b/c/age.txt') // 解析的是传入文件的所有父级路径,只是不展示当前文件的路径
+            let extendName = path.extname('a.js') // 取扩展名,就算是a.min.js 也是取.js 
+
+            // console.log(currentPath);
+            // console.log(currentPath1);
+            // console.log(parent);
+            // console.log(extendName);
+
+
+// vm runInThisContext (运行在这个上下文,沙盒)
+// path resolve(解析路径) join(拼接路径) dirname(父路径) extname(拓展名)
+// fs readSync(同步读取文件) existSync(同步测试文件是否存在,boolean)
+
+// 遇到难题, 1. 先自己编写 2. 看源码 3. 再编 4. 看源码
--- 架构/6.module/useA.js
+++ 架构/6.module/useA.js
@@ -1,5 +1,58 @@
-let xx = require('./a.js')
-console.log(xx);
-setTimeout(()=>{
-    console.log(xx);
-},1500)
+let result = require('./a.js')
+console.log(result);
+// setTimeout(()=>{
+//     console.log(result);
+// },1500)
+
+
+// 断点调试的几种方式
+// 1) google浏览器 适合第三方模块的调试(比如webpack)
+    // 1. 命令行执行:node --inspect-brk 文档名.js 
+    // 2. 浏览器行输入: chrome://inspect
+        // 就会出现一个链接,如果没有就在执行第一步一次,有个 open dedicated devtools for node 这句话下面有个链接 就直接开启
+    // 3. 可以右键属性 有个add select text 就会在右侧展示,可以看里面的属性
+
+
+// 测试require的流程(并且学习看源码)
+/* 
+1. Module.prototype.require 要实现一个require方法 在useA模块中(一个文件就是一个模块,useA就是一个模块,模块有一个自己的原型上的方法require),使用了a.js文件
+2. Module._load 加载模块
+3. 先查看是否有缓存,有缓存就使用,没有再加载
+4. 如果没有缓存,就直接将模块路径转换成绝对路径 ,用这个方法Module._resolveFilename
+5. 根据转换的路径,查看是否有缓存机制,如果没有缓存看下是否是原生模块
+
+6. 创建new Module(没查到,就创建模块)
+ // new了一类 就是创造一个对象
+ // 对象里面有 id = 文件路径
+ // exports 就是最后导出的文件
+    /* 
+    function Module(id = '', parent) {
+    this.id = id;
+    this.path = path.dirname(id);
+    this.exports = {};
+    this.parent = parent;
+    updateChildren(parent, this, false);
+    this.filename = null;
+    this.loaded = false;
+    this.children = [];
+    }
+
+7. 把模块缓存起来,为了下次使用时可以使用上次缓存的模块
+8. module.load加载模块
+9. 拿到文件拓展名.js 调用对应的模块解析规则
+10. 读到文件模块 编译模块文件
+11. 包装成函数 并且让函数执行 模块的this指向是exports
+*/
+
+
+
+
+
+
+*/
+// 看源码的步骤非常多
+/* 
+1. 先看面  => 整体流程 一般是看看目录名字,目录描述
+2. 感兴趣  => 就点进去看
+3. 工具类的方法不看怎么实现
+*/
