713f5b9ec0e4a11ec1f0f6a546b80fe75ba5f0d2
tkf <kaifang.tang@henganpros.com>
2020-12-21 22:24:23 +0800

    1

---
+++ 有用的/常见面试题及解法/斐波那契.js
@@ -0,0 +1,18 @@
+let fb = function(len){
+    if(typeof len !== 'number')  throw('需要输入数字')
+    let arr = [0,1]
+    let num = 0
+    for(let i =2;i<len;i++){
+        arr.push(arr[i-2]+arr[i-1])
+    }
+    for(let k = 0;k<arr.length;k++){
+        num+=arr[k]
+    }
+    return {
+        count: num,
+        fb:arr
+    }
+
+    console.log('是数字');
+}
+console.log(fb(0));
